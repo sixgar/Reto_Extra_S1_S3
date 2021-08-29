cf03cf7e6b2089dcd86f31031af07d20bdc84220
sixgar <sixgar05178@gmail.com>
2021-08-28 22:32:22 -0500

    Insertando un nuevo aerchivo al repositorio

---
+++ Reto1.py
@@ -0,0 +1,22 @@
+print("Esto es una prueba")
+Numero_uno = 7
+Numero_dos = 13
+Resultado = (Numero_uno + Numero_dos)
+print(Resultado)
+
+print("esto es un mensaje")
+Mensaje = ("hola ernesto")
+Busqueda = Mensaje.find("ernesto")
+print(Busqueda)
+
+print("extraer cadena de texto")
+Extraer = Mensaje[5:12]
+print(Extraer)
+
+print("Comparación")
+palabra_uno = "hola"
+palabra_dos = "carro"
+print(palabra_uno==palabra_dos)
+
+print(Numero_dos - Numero_uno)
+
